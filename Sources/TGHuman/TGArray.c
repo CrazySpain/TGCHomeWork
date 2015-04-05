cc21ef952ab80d35a0fb7cbbc1a9a089c14bfb28
CrazySpain <toraholdman@gmail.com>
2015-04-05 18:34:52 +0300

    Added new methods. Refactor.

--- Sources/TGHuman/TGArray.c
+++ Sources/TGHuman/TGArray.c
@@ -13,36 +13,30 @@
 #include <assert.h>
 
 #pragma mark -
+#pragma mark Private Declaration
+
+void TGArraySetCount(TGArray *array, size_t count);
+
+void TGArraySetObjectAtIndex(TGArray *array, void *object, uint32_t index);
+
+#pragma mark -
 #pragma mark Accessors
 
-void *TGArrayGetObjectAtIndex(TGArray *array, uint32_t index) {
-    if ((NULL == array) || (index >= array->_length) || (NULL == array->_array)) {
-        return NULL;
-    }
-    
-    return array->_array[index];
-}
-
-void TGArraySetObject(TGArray *array, void *object) {
-    if (NULL == array) {
-        return;
-    }
-    
-    TGArraySetLength(array, (array->_length + 1));
-    
-    array->_array[array->_length - 1] = TGObjectRetain(object);
-}
-
 size_t TGArrayGetLength(TGArray *array) {
-    return array->_length;
+    if (NULL != array) {
+        return array->_length;
+    }
+    
+    return 0;
 }
 
 void TGArraySetLength(TGArray *array, size_t length) {
+    size_t currentLength = TGArrayGetLength(array);
+    for (uint32_t index = (uint32_t)length; index < currentLength; index++) {
+        TGObjectRelease(TGArrayGetObjectAtIndex(array, index));
+    }
+    
     if (0 == length) {
-        for (int index = 0; index < array->_length; index++) {
-            TGObjectRelease(array->_array[index]);
-        }
-        
         free(array->_array);
         array->_array = NULL;
         array->_length = 0;
@@ -50,37 +44,39 @@ void TGArraySetLength(TGArray *array, size_t length) {
         return;
     }
     
-    if (array->_length != length) {
-        if (array->_length > length) {
-            for (size_t index = length; index < array->_length; index++) {
-                TGObjectRelease(array->_array[index]);
-            }
-        }
-        
+    if (currentLength != length) {
         array->_array = realloc(array->_array, length * sizeof(*array));
         
         assert(array->_array != NULL);
         
-        if (array->_length < length) {
-            memset(array->_array + array->_length, 0, length - array->_length);
+        if (currentLength < length) {
+            memset(array->_array + currentLength, 0, length - currentLength);
         }
         
         array->_length = length;
     }
 }
 
+size_t TGArrayGetCount(TGArray *array) {
+    if (NULL != array) {
+        return array->_count;
+    }
+    
+    return 0;
+}
+
+void TGArraySetCount(TGArray *array, size_t count) {
+    if (NULL != array) {
+        array->_count = count;
+    }
+}
+
 #pragma mark -
 #pragma mark Public Implementation
 
-TGArray *TGArrayCreateWithObject(void *object) {
-    if (NULL == object) {
-        return NULL;
-    }
-    
+TGArray *TGArrayCreate() {
     TGArray *array = TGObjectCreate(TGArray);
-    array->_array = (void **)calloc(1, sizeof(object));
-    
-    TGArraySetObject(array, object);
+    array->_array = (void **)calloc(1, sizeof(TGArray));
     
     return array;
 }
@@ -93,23 +89,64 @@ void __TGArrayDealloc(TGArray *array) {
     __TGObjectDealloc(array);
 }
 
-void TGArrayRemoveObject(TGArray *array, void *object) {
+void *TGArrayGetObjectAtIndex(TGArray *array, uint32_t index) {
+    if ((NULL == array) || (index >= TGArrayGetCount(array)) || (NULL == array->_array)) {
+        return NULL;
+    }
+    
+    return array->_array[index];
+}
+
+void TGArrayAddObject(TGArray *array, void *object) {
+    if (NULL == array) {
+        return;
+    }
+    
+    size_t length = TGArrayGetLength(array) + 1;
+    
+    TGArraySetLength(array, length);
+    TGArraySetObjectAtIndex(array, object, (uint32_t)(length - 1));
+}
+
+uint32_t TGArrayIndexOfObject(TGArray *array, void *object) {
     if ((NULL != array) && (NULL != array->_array)) {
-        for (int index = 0; index < array->_length; index++) {
-            if (array->_array[index] == object) {
-                TGArrayRemoveObjectAtIndex(array, index);
+        size_t count = TGArrayGetCount(array);
+        for (int index = 0; index < count; index++) {
+            if (TGArrayGetObjectAtIndex(array, index) == object) {
+                return index;
             }
         }
     }
+    
+    return 0;
+}
+
+void TGArrayRemoveObject(TGArray *array, void *object) {
+    TGArrayRemoveObjectAtIndex(array, TGArrayIndexOfObject(array, object));
 }
 
 void TGArrayRemoveObjectAtIndex(TGArray *array, uint32_t index) {
-    if ((NULL != array) && (NULL != array->_array) && (index < array->_length)) {
+    size_t count = TGArrayGetCount(array);
+    if ((NULL != array) && (NULL != array->_array) && (index < count)) {
         TGObjectRelease(array->_array[index]);
-        for (int iterator = index; iterator < (array->_length - 1); iterator++) {
+        for (int iterator = index; iterator < (count - 1); iterator++) {
             array->_array[iterator] = array->_array[iterator + 1];
         }
         
-        TGArraySetLength(array, (array->_length - 1));
+        TGArraySetLength(array, (TGArrayGetLength(array) - 1));
+        TGArraySetCount(array, (TGArrayGetCount(array) - 1));
+    }
+}
+
+#pragma mark -
+#pragma mark Private Implementation
+
+void TGArraySetObjectAtIndex(TGArray *array, void *object, uint32_t index) {
+    size_t count = TGArrayGetLength(array);
+    
+    if ((NULL != array) && (NULL != object) && (index < count)) {
+        TGObjectRelease(TGArrayGetObjectAtIndex(array, index));
+        array->_array[array->_length - 1] = TGObjectRetain(object);
+        TGArraySetCount(array, (TGArrayGetCount(array) + 1));
     }
 }
--- Sources/TGHuman/TGArray.h
+++ Sources/TGHuman/TGArray.h
@@ -20,25 +20,32 @@ struct TGArray {
     
     void **_array;
     size_t _length;
+    size_t _count;
 };
-
-extern
-TGArray *TGArrayCreateWithObject(void *object);
-
-extern
-void __TGArrayDealloc(TGArray *array);
-
-extern
-void *TGArrayGetObjectAtIndex(TGArray *array, uint32_t index);
-
-extern
-void TGArraySetObject(TGArray *array, void *object);
 
 extern
 size_t TGArrayGetLength(TGArray *array);
 
 extern
 void TGArraySetLength(TGArray *array, size_t length);
+
+extern
+size_t TGArrayGetCount(TGArray *array);
+
+extern
+void __TGArrayDealloc(TGArray *array);
+
+extern
+TGArray *TGArrayCreate();
+
+extern
+void *TGArrayGetObjectAtIndex(TGArray *array, uint32_t index);
+
+extern
+void TGArrayAddObject(TGArray *array, void *object);
+
+extern
+uint32_t TGArrayIndexOfObject(TGArray *array, void *object);
 
 extern
 void TGArrayRemoveObject(TGArray *array, void *object);
