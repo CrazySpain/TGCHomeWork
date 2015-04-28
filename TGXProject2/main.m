5788639e5661ba83793aa8332989e3eed2d7497f
CrazySpain <toraholdman@gmail.com>
2015-04-28 16:39:33 +0300

    Created 20 creatures.

--- TGXProject2/main.m
+++ TGXProject2/main.m
@@ -8,10 +8,41 @@
 
 #import <Foundation/Foundation.h>
 
+#import "TGHomoSapiens.h"
+#import "TGMan.h"
+#import "TGWoman.h"
+
+#import "NSObject+TGExtensions.h"
+
+static const NSInteger kTGHomoSapiensesCount    = 20;
+static const NSInteger kTGGendersCount          = 2;
+
+typedef NS_ENUM(NSInteger, TGGender) {
+    TGHe = 0,
+    TGShe
+};
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSMutableArray *creatures = [NSMutableArray array];
+        
+        for (NSInteger count = 0; count < kTGHomoSapiensesCount; count++) {
+            NSInteger gender = arc4random_uniform(kTGGendersCount);
+            TGHomoSapiens *homoSapiens = nil;
+            
+            if (gender == TGHe) {
+                homoSapiens = [TGMan object];
+            } else if (gender == TGShe) {
+                homoSapiens = [TGWoman object];
+            }
+            
+            [creatures addObject:homoSapiens];
+        }
+        
+        for (TGHomoSapiens *homoSapiens in creatures) {
+            [homoSapiens performGenderSpecificOperation];
+        }
     }
+    
     return 0;
 }
