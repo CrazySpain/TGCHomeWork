84c03b1d2495093d1c80614a2ce4f075fe675ec4
CrazySpain <toraholdman@gmail.com>
2015-05-03 13:59:49 +0300

    Added new target.

---
+++ TGXProject3/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TGXProject3
+//
+//  Created by Tora on 5/3/15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    
+    return 0;
+}
